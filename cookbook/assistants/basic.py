2eb9f19e01427447c1c5411f6f08bce923fc9a40
Austin <austin.soetopo@gmail.com>
2024-05-16 18:08:48 +0700

    Changed basic.py, finance.py, and sahamology.py

--- cookbook/assistants/basic.py
+++ cookbook/assistants/basic.py
@@ -1,10 +1,12 @@
 from phi.assistant import Assistant
 # from phi.llm.openai import OpenAIChat
-from phi.llm.ollama import Ollama
+# from phi.llm.ollama import Ollama
+from phi.llm.groq import Groq
 
 assistant = Assistant(
     # llm=OpenAIChat(model="gpt-4o"),
-    llm=Ollama(model='llama3'),
+    # llm=Ollama(model='llama3'),
+    llm=Groq(model='llama3-70b-8192'),
     description="You help people with their health and fitness goals.",
     instructions=["Recipes should be under 5 ingredients"],
 )
--- cookbook/assistants/finance.py
+++ cookbook/assistants/finance.py
@@ -1,12 +1,21 @@
+from pathlib import Path
+from textwrap import dedent
+
 from phi.assistant import Assistant
 from phi.llm.openai import OpenAIChat
 from phi.tools.yfinance import YFinanceTools
 
+cwd = Path(__file__).parent.resolve()
+scratch_dir = cwd.joinpath("scratch")
+if not scratch_dir.exists():
+    scratch_dir.mkdir(exist_ok=True, parents=True)
+
 assistant = Assistant(
     llm=OpenAIChat(model="gpt-4o"),
     tools=[YFinanceTools(stock_price=True, analyst_recommendations=True, company_info=True, company_news=True)],
-    show_tool_calls=True,
+    # show_tool_calls=True,
     markdown=True,
+    save_output_to_file=str(scratch_dir.joinpath("new_report.md"))
 )
-assistant.print_response("What is the stock price of NVDA")
+# assistant.print_response("What is the stock price of NVDA")
 assistant.print_response("Write a comparison between NVDA and AMD, use all tools available.")
